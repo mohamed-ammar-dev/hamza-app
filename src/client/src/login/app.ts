1fc0e03a514e111df08b6e974762a629f9c4051e
mohamed-ammar-dev <mohamed.ammar.dev@gmail.com>
2021-07-27 18:34:49 +0200

    Fix production bug

--- src/client/src/login/app.ts
+++ src/client/src/login/app.ts
@@ -1,8 +1,6 @@
 import { Images } from "./images/images";
 import { Login } from "./login";
 
-console.log(document.cookie);
-
 if (document.cookie) document.location.href = "/home";
 
 new Images();
